8b92c2d6b197e19f2032b4f32d45ca271885c961
vishal lazrus <vishallazrus3210@gmail.com>
2021-10-10 23:51:31 +0530

    commited

---
+++ Pizza_bill.py
@@ -0,0 +1,25 @@
+print("Welcome To 🍕Pizza Deliveries! ")
+size=input("What size Pizza 🍪🍕 do you want? S, M, L -> ")
+add_pepperoni =input("Do yout want pepperoni? Y or N -> ")
+
+
+bill=0
+if size =="S":
+    bill +=15
+elif size =="M":
+    bill +=20
+else:
+    bill +=25
+
+if add_pepperoni =="Y":
+    pepperoni_size=input("What size Pepperoi you want? S or L -> ")
+    if size =="S":
+        bill +=2
+    else:
+        bill +=3
+        
+extra_cheese= input("Do you want extra cheese? Y or N -> ")
+if extra_cheese =="Y":
+    bill +=1
+
+print(F"🍪🍕your final bill is: ${bill}.")
